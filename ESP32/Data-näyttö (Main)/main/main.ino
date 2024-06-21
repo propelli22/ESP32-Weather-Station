c2d1283ff01043a5161b6e420be15ded927c06cf
Kalle Kahri <149298638+propelli22@users.noreply.github.com>
2024-06-21 18:45:11 +0300

    Data näytön koodia jatkettu
    Toiselle näytölle datan jatkaminen ei tehty, koodia ei myöskään testattu joten virheitä varmaa miljoona :)

--- ESP32/Data-näyttö (Main)/main/main.ino
+++ ESP32/Data-näyttö (Main)/main/main.ino
@@ -1,9 +1,42 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <esp_now.h>
+#include <TFT_eSPI.h>
+#include <SPI.h>
+#include "pin_config.h"
 
-const char* ssid = "DNA-WIFI-62C7";
-const char* password = "48140406";
-const char* serverUrl = "http://192.168.1.123:3000/data"; // Replace with your server URL
+TFT_eSPI tft = TFT_eSPI();
+
+unsigned long targetTime = 0;
+byte red = 31;
+byte green = 0;
+byte blue = 0;
+byte state = 0;
+unsigned int colour = red << 11;
+uint32_t runing = 0;
+
+const char* ssid = "";
+const char* password = "";
+const char* serverUrl = "";
+
+float saapuvaLampotila;
+
+float lampotilaTulostus;
+
+typedef struct data_paketti {
+  float vesiLampotila;
+} data_paketti;
+
+data_paketti incomingReadings;
+
+esp_now_peer_info_t peerInfo;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t * incomingData, int len) {
+  memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  saapuvaLampotila = incomingReadings.vesiLampotila;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -15,6 +48,30 @@ void setup() {
   }
   
   Serial.println("Connected to WiFi");
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  pinMode(PIN_POWER_ON, OUTPUT);
+  digitalWrite(PIN_POWER_ON, HIGH);
+
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+
+  targetTime = millis() + 1000;
 }
 
 void sendData(float sensorData) {
@@ -47,9 +104,44 @@ void sendData(float sensorData) {
 }
 
 void loop() {
-  float testValue = 14.0; // Fixed test value
-  
-  sendData(testValue);
-  
-  delay(10000); // Send data every minute
+  sendData(saapuvaLampotila);
+
+  lampotilaTulostus = saapuvaLampotila + "°C"
+
+  if (millis() > runing) {
+    Serial.print("Current running ");
+    Serial.print(millis());
+    Serial.println(" millis");
+    runing = millis() + 1000;
+  }
+
+  tft.drawCentreString(lampotilaTulostus, 80, 92, 6);
 }
+
+// TFT Pin check
+#if PIN_LCD_WR  != TFT_WR || \
+    PIN_LCD_RD  != TFT_RD || \
+    PIN_LCD_CS    != TFT_CS   || \
+    PIN_LCD_DC    != TFT_DC   || \
+    PIN_LCD_RES   != TFT_RST  || \
+    PIN_LCD_D0   != TFT_D0  || \
+    PIN_LCD_D1   != TFT_D1  || \
+    PIN_LCD_D2   != TFT_D2  || \
+    PIN_LCD_D3   != TFT_D3  || \
+    PIN_LCD_D4   != TFT_D4  || \
+    PIN_LCD_D5   != TFT_D5  || \
+    PIN_LCD_D6   != TFT_D6  || \
+    PIN_LCD_D7   != TFT_D7  || \
+    PIN_LCD_BL   != TFT_BL  || \
+    TFT_BACKLIGHT_ON   != HIGH  || \
+    170   != TFT_WIDTH  || \
+    320   != TFT_HEIGHT
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#error  "Error! Please make sure <User_Setups/Setup206_LilyGo_T_Display_S3.h> is selected in <TFT_eSPI/User_Setup_Select.h>"
+#endif
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5,0,0)
+#error  "The current version is not supported for the time being, please use a version below Arduino ESP32 3.0"
+#endif
